bb0bb89088b0c0c2af4a427447704b29979ede24
Jamie Thompson <jlthompson76@users.noreply.github.com>
2020-03-28 17:21:29 -0400

    Finish Bonus 3 and modify function parameters and arguments

--- script.js
+++ script.js
@@ -8,10 +8,10 @@
 // Hint: Try using a loop! Declare the function, then call the function in the same JavaScript file, inside a console.log() statement. Open the Console to display your results.
 
 console.log(`#1 Sum of an array`);
-let numbers = [3, 4, 5];
-console.log(sum());
+console.log(sum([3, 4, 5]));
+console.log(sum([10, 5, 9]));
 
-function sum() {
+function sum(numbers) {
     let sum = 0;
     for (let i = 0; i < numbers.length; i++) {
         sum += numbers[i];
@@ -28,10 +28,10 @@ function sum() {
 // Hint: calculating the average requires only one more step after finding the sum.
 
 console.log(`#2 Average of an array`);
-numbers = [10, 5, 9];
-console.log(average());
+console.log(average([3, 4, 5]));
+console.log(average([10, 5, 9]));
 
-function average() {
+function average(numbers) {
     let sum = 0;
     for (i = 0; i < numbers.length; i++) {
         sum += numbers[i];
@@ -43,10 +43,11 @@ function average() {
 // Now, rewrite your `average` function so it *calls your `sum` function* to add the numbers up. This is an example of using a function to reuse code.
 
 console.log(`Bonus 1`);
-console.log(averageBonus());
+console.log(averageBonus([3, 4, 5]));
+console.log(averageBonus([10, 5, 9]));
 
-function averageBonus() {
-    average = sum() / numbers.length;
+function averageBonus(numbers) {
+    average = sum(numbers) / numbers.length;
     return average;
 }
 
@@ -57,10 +58,9 @@ function averageBonus() {
 // max([7,10, 30, 1]); // returns 30
 
 console.log(`Bonus 2`);
-numbers = [7, 10, 30, 1];
-console.log(max());
+console.log(max([7, 10, 30, 1]));
 
-function max() {
+function max(numbers) {
     let max = numbers[0];
     for (let i = 0; i < numbers.length; i++) {
         if (max < numbers[i]) {
@@ -78,8 +78,31 @@ function max() {
 // largestSum([3, 2, 8], [4, 9, 1]); // returns 17
 
 console.log(`Bonus 3`);
-console.log(largestSum());
+console.log(largestSum([3, 2, 8], [4, 9, 1]));
 
-function largestSum() {
+function largestSum(numbers1, numbers2) {
     
+    let max1 = max(numbers1);
+    let max2 = max(numbers2);
+    console.log(max1, max2);
+    let largestSum = max1 + max2;
+    return largestSum;
+
+    function max(numbers) {
+        let max = numbers[0];
+        for (let i = 0; i < numbers.length; i++) {
+            if (max < numbers[i]) {
+                max = numbers[i];
+            } else {
+                max = max;
+            }
+        } return max;
+    }
+
+    function sum(numbers) {
+        let sum = 0;
+        for (let i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        } return sum;
+    }
 }
